fec27d764988f3c877cf3034c77d6f1c0c51ed76
zithron <abdulbasithb70@gmail.com>
2017-03-30 09:50:22 +0700

    update index

--- js/joints.js
+++ js/joints.js
@@ -1,7 +1,47 @@
 $(document).ready(function () {
-    
     var $svg = $('svg').drawsvg();
 
     $svg.drawsvg('animate');
 
+    var $svg = $('.svg').drawsvg({
+        easing: 'easeInOutQuart',
+        callback: function () {
+            animate();
+        }
+    });
+    
+    var $form = $('.form svg').drawsvg({
+        easing: "easeInOutQuart",
+    });
+    var $monitor = $('.monitor svg').drawsvg({
+        easing: "easeInOutQuart",
+    });
+    var $piala = $('.piala svg').drawsvg({
+        easing: "easeInOutQuart",
+    });
+    /*RUN*/
+    function animate() {
+        $svg.drawsvg('animate');
+    }
+    animate();
+    var options = [{
+        selector: '.form',
+        offset: 300,
+        callback: function () {
+            $form.drawsvg('animate');
+        }
+    }, {
+        selector: '.monitor',
+        offset: 300,
+        callback: function () {
+            $monitor.drawsvg('animate');
+        }
+    }, {
+        selector: '.piala',
+        offset: 300,
+        callback: function () {
+            $piala.drawsvg('animate');
+        }
+    }, ];
+    Materialize.scrollFire(options);
 });
